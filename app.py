03bf1dd7f8464d6e3182fcf5093ededbd17f0af9
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-12-30 11:16:52 -0500

    new domain stuff

---
+++ app.py
@@ -0,0 +1,43 @@
+from sanic import response, Sanic
+
+from functools import wraps
+
+import aiohttp
+
+import json
+import os
+
+app = Sanic(__name__)
+
+with open('data/status_codes.json') as f:
+    app.status_codes = json.load(f)
+
+
+def authorized():
+    def decorator(f):
+        @wraps(f)
+        async def decorated_function(request, *args, **kwargs):
+            if request.token == os.environ.get('auth-token'):
+                return await f(request, *args, **kwargs)
+            return response.json({'error': True, 'message': 'Unauthorized'}, status=401)
+        return decorated_function
+    return decorator
+
+
+@app.listener('before_server_start')
+async def create_session(app, loop):
+    app.session = aiohttp.ClientSession(loop=loop)
+
+
+@app.listener('after_server_stop')
+async def close_session(app, loop):
+    await app.session.close()
+
+
+@app.route('/')
+async def index(request):
+    return response.json({'hello': 'world'})
+
+
+if __name__ == '__main__':
+    app.run(port=8080)
