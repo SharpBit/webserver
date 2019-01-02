e8030d85c50632294f48b97dec7b1be1e0e62285
SharpBit <31069084+SharpBit@users.noreply.github.com>
2019-01-01 21:25:29 -0500

    more print debugging

--- app.py
+++ app.py
@@ -86,10 +86,10 @@ async def login(request):
 @app.get('/callback')
 async def callback(request):
     code = request.raw_args.get('code')
+    print(code)
     access_token, expires_in = await app.oauth.get_access_token(code)
     print(access_token, expires_in)
     user = await app.oauth.get_user_json(access_token)
-    print(user)
 
     data = {
         'name': user['username'],
