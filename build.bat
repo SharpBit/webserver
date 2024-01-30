398dcba0df36a1e60fdf4cc6a4bdcc8bea890334
SharpBit <31069084+SharpBit@users.noreply.github.com>
2024-01-29 20:06:24 -0500

    use docker compose to build now (fixes env issue)

--- build.bat
+++
@@ -1,2 +0,0 @@
-docker build -t sharpbitdev .
-docker run -d --name sharpbitdev -p 127.0.0.1:4000:4000 sharpbitdev
--- build.sh
+++
@@ -1,2 +0,0 @@
-docker build -t sharpbitdev .
-docker run -d --name sharpbitdev -p 4000:4000 sharpbitdev
